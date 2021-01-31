224ef6e5f2602c20e668c4b1adb9cadb50ea7866
berfinkosemen <berfinkosemen15@gmail.com>
2021-01-31 15:42:34 +0300

    second_commit

---
+++ src/MasterYoda.cpp
@@ -0,0 +1,24 @@
+
+
+#include "MasterYoda.h"
+
+MasterYoda::MasterYoda() {
+	setCharacterType("iyi");
+
+	health = 6;
+}
+
+MasterYoda::~MasterYoda()
+{
+
+}
+
+int MasterYoda::getHealth() {
+	return health;
+}
+
+void MasterYoda::setHealth(int health) {
+	this->health = health;
+}
+
+
